9678b6c900d66cd0049a787745645bfef1d64c1d
Andrew Nelson <ajnels100@msn.com>
2021-11-07 19:53:32 -0500

    Create a very simple lexicon

---
+++ main.py
@@ -0,0 +1,47 @@
+import csv
+import os
+
+language = ''
+
+
+def welcome_prompt():
+    print('Welcome to Lexicon!')
+    print('Change Language')
+    print('Add Word')
+    user_input = input('Enter choice\n')
+
+    if user_input.lower() == 'a':
+        add_word()
+    elif user_input.lower() == 'l':
+        change_language()
+    elif user_input.lower() == 'q':
+        exit()
+
+    welcome_prompt()
+
+
+def add_word():
+    if language == '':
+        print('Please define a language first')
+        change_language()
+
+    word = input('Word: ')
+    part_of_speech = input('Part of Speech: ')
+    definition = input('Definition: ')
+
+    if not os.path.exists(language) or not os.path.isdir(language):
+        os.mkdir(language)
+
+    with open(language + '/test.csv', 'a', newline='') as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow([word, part_of_speech, definition])
+    csv_file.close()
+
+
+def change_language():
+    global language
+    language = input("Enter Language to work on:\n")
+
+
+if __name__ == '__main__':
+    welcome_prompt()
